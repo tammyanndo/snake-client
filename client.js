5e65539f7cb0b078536f8bceed77f55b06f5de08
Tammy <tammyanndo@gmail.com>
2021-09-23 14:10:36 -0400

    added constants.js

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { host, port } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
+    host,
+    port
   });
 
   conn.setEncoding("utf8");
