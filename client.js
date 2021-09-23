7b49e209d6b718e7f0e0db167244554291a33cff
Tammy <tammyanndo@gmail.com>
2021-09-22 20:06:19 -0400

    updated with name

--- client.js
+++ client.js
@@ -1,15 +1,19 @@
 const net = require("net");
+
 const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133',
     port: 50542
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    console.log('connected');
+    console.log('Successfully connected to game server');
+    });
+
+    conn.on('connect', () => {
+      conn.write('Name: TAM');
     });
 
   conn.on('data', (data) => {
